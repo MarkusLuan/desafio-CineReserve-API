0f98df2960bab0e61e22e2ae78181c751d913fce
markusluan <markus23_luan@hotmail.com>
2026-03-19 12:43:32 -0300

    Criando a base do flask e do singleton

--- app/src/app/main.py
+++ app/src/app/main.py
@@ -1,11 +1,26 @@
 from flask import Flask
 
 from .error_handler import ErrorHandler
+from .resources import resources
+from . import app_singleton
 
-app = Flask("CineReserve")
-ErrorHandler(app)
+def create_app():
+    _app = Flask("CineReserve")
+    ErrorHandler(_app)
+
+    # TODO: Criar config do banco
+    # app_singleton.db.init_app(_app)
+    # app_singleton.migrate.init_app(_app, app_singleton.db)
+
+    app_singleton.jwt.init_app(_app)
+    app_singleton.basic_auth.init_app(_app)
+
+    _app.register_blueprint(resources)
+    return _app
 
 def main():
+    app = create_app()
+
     app.run(
         host="0.0.0.0",
         port=82,
